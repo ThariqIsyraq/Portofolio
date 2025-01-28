84edf97f73edb7709d681e07c4ea4cbf06389dc8
ThariqIsyraq <thariqisyraq09@gmail.com>
2025-01-28 22:56:47 +0700

    Telah menambahkan feature slide saat tombol view ditekan untuk menunjukkan informasi yang lebih banyak terkait profile saya

--- JS/index.js
+++ JS/index.js
@@ -4,16 +4,32 @@ feather.replace();
 // Memanggil Element
 const profilePicture = document.querySelector('.profile-picture');
 const openBtn = document.getElementById('profile-btn');
+const popup = document.getElementById('side-profile');
 
 // Animasi hover pada profile button
 profilePicture.addEventListener('mouseover', () => {
-    openBtn.style.width = '105px'; 
+    openBtn.style.width = '105px';
 });
 
 profilePicture.addEventListener('mouseout', () => {
-    openBtn.style.width = '100px'; 
+    openBtn.style.width = '100px';
 });
 
 openBtn.addEventListener('mouseover', () => {
-    openBtn.style.width = '105px'; 
+    openBtn.style.width = '105px';
 });
+
+// Sidebar slide feature
+openBtn.addEventListener('click', () => {
+    popup.classList.add('show');
+    popup.classList.remove('hidden')
+});
+
+document.addEventListener('click', (e) => {
+    const clicked = popup.contains(e.target) || openBtn.contains(e.target);
+
+    if (!clicked) {
+        popup.classList.remove('show');
+        popup.classList.add('hidden');
+    }
+})
