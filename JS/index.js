14642d0bbc05cfde6eb025087f1b19d9ad45f50c
ThariqIsyraq <thariqisyraq09@gmail.com>
2025-01-28 22:41:14 +0700

    Menyelesaikan Main Profile

--- JS/index.js
+++ JS/index.js
@@ -1,2 +1,19 @@
 // FeatherIcons
 feather.replace();
+
+// Memanggil Element
+const profilePicture = document.querySelector('.profile-picture');
+const openBtn = document.getElementById('profile-btn');
+
+// Animasi hover pada profile button
+profilePicture.addEventListener('mouseover', () => {
+    openBtn.style.width = '105px'; 
+});
+
+profilePicture.addEventListener('mouseout', () => {
+    openBtn.style.width = '100px'; 
+});
+
+openBtn.addEventListener('mouseover', () => {
+    openBtn.style.width = '105px'; 
+});
